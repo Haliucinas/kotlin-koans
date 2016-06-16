5d7c8135529f69daef46f2297f489ae55daf58c7
Mantas Jonytis <mantas.jonytis@mediapark.lt>
2016-06-16 17:05:21 +0300

    Collections solutions

--- src/ii_collections/_21_Partition_.kt
+++ src/ii_collections/_21_Partition_.kt
@@ -12,5 +12,8 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return this.customers.filter({
+        val (neg, pos) = it.orders.partition({ it.isDelivered })
+        pos.count() > neg.count()
+    }).toSet()
 }
